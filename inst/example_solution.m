766ffdfbc3814851b1cbdcb4f7c7a1850f04fa95
carlodefalco <carlodefalco@users.noreply.github.com>
2020-06-26 11:10:50 +0200

    Update example_solution.m

--- inst/example_solution.m
+++ inst/example_solution.m
@@ -1,9 +1,20 @@
 function [u, ux, ut] = example_solution (t, x)
 
   coefficients
+
+
   xi  = t/T;
   xit = 1/T;
   u   =  ((x-L).^2/(L^2/(1-xi)) + xi);
   ux  =  (2*(1 - t/T) .* (-L + x)) / L^2;
   ut  =  1/T - (-L + x).^2 ./(L.^2 * T);
+
+%{
+  kappa = (omega/A)*(L/T);
+  u   = sin ( 2*pi * (kappa*x/L-omega*t/T) );
+  ut  = -2*pi*omega/T * cos ( 2*pi * (kappa*x/L-omega*t/T) );
+  ux  = 2*pi*kappa/L * cos ( 2*pi * (kappa*x/L-omega*t/T) );
+%}
+
 endfunction
+
