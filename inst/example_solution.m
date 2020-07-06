94a5604c44ace6c60744ec4c3831831b7acd3a08
MarcoFois <67022369+MarcoFois@users.noreply.github.com>
2020-07-06 12:37:58 +0200

    Update example_solution.m
    stationary case added

--- inst/example_solution.m
+++ inst/example_solution.m
@@ -8,11 +8,25 @@ function [u, ux, ut] = example_solution (t, x)
     u   =  ((x-L).^2/(L^2/(1-xi)) + xi);
     ux  =  (2*(1 - t/T) .* (-L + x)) / L^2;
     ut  =  1/T - (-L + x).^2 ./(L.^2 * T);
+    
   elseif (testnum == 2)
+  
     kappa = (omega/A)*(L/T);
     u   = sin ( 2*pi * (kappa*x/L-omega*t/T) );
     ut  = -2*pi*omega/T * cos ( 2*pi * (kappa*x/L-omega*t/T) );
     ux  = 2*pi*kappa/L * cos ( 2*pi * (kappa*x/L-omega*t/T) );
-  end
   
+   elseif (testnum == 3)
+      
+    u = t/T.*exp(sin(L*x));
+    ut = 1/T.*exp(sin(L*x));
+    ux = t/T*L*cos(L*x).*exp(sin(L*x));
+    
+  elseif (testnum == 4)
+      
+    u = 3*sin(x.^2);
+    ut = zeros(size(x));
+    ux = 6*x.*cos(x.^2);
 end
+
+return
