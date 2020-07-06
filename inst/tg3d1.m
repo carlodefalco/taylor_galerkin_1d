a048888c1532e0b3afb6972ff20e5fe41c8c3294
MarcoFois <67022369+MarcoFois@users.noreply.github.com>
2020-07-06 12:56:09 +0200

    Update tg3d1.m

--- inst/tg3d1.m
+++ inst/tg3d1.m
@@ -6,7 +6,7 @@ function d1 = tg3d1 (msh, u, A, s)
       for j = 1 : 2     
         d1(msh.conn(i, k)) = d1(msh.conn(i, k)) + ...
         - A * u(msh.conn(j, k)) * msh.shg(j, k) * msh.shp(i, i, k) * msh.h(k)/2 + ... %% advection
-          s(msh.conn(i, k)) * msh.h(k) * msh.shp(i, j, k)  / 2;                       %% source (non so se sia coerente nel metodo di ordine 3)
+          
       end
     end
   end
